355d4c97676c7d6e687523c1236607f1834934a6
Kayque546 <kayqueln@gmail.com>
2022-04-25 20:16:40 -0300

    commit

--- app/src/main/java/br/com/etecia/atividade/MainActivity.java
+++ app/src/main/java/br/com/etecia/atividade/MainActivity.java
@@ -3,12 +3,36 @@ package br.com.etecia.atividade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //Declarar os componentes utilizados
+    EditText edtlogin, edtSenha;
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declarando os componentes
+        edtlogin = findViewById(R.id.edtLogin);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        //Eventos de clique
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void acessarJanela(View view) {
+
+
     }
 }
