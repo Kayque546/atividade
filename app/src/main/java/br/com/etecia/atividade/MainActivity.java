8bdda932e6af11c4a8a24fa6133afb0df261f222
Kayque546 <kayqueln@gmail.com>
2022-04-27 20:22:53 -0300

    commit

--- app/src/main/java/br/com/etecia/atividade/MainActivity.java
+++ app/src/main/java/br/com/etecia/atividade/MainActivity.java
@@ -2,10 +2,12 @@ package br.com.etecia.atividade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Declarar os componentes utilizados
@@ -26,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String login = edtlogin.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                if(login.equals("eteccia") && senha.equals("etec123")){
+                   //abrindo outra janela
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos"+ edtlogin, Toast.LENGTH_SHORT).show();
+                }
+
+
 
             }
         });
